9b6253a1138d7ad4f01cbfa6a41be22f3b93c8fe
Henry Kam <h3nrykam@gmail.com>
2021-09-29 17:08:06 -0700

    finish jquery logic

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 var curTime = parseInt(moment().format("HH"));
 var lim = -1;
+
 $(".description").each(function(index, element) {
     if(curTime-9<index) 
        $(element).attr("class", "future col-md-10");
     else if(curTime-9 == index)
+    {
         $(element).attr("class", "present col-md-10");
+        $(element).text("Current Hour");
+    }  
     else
         $(element).attr("class", "past col-md-10");
 });
+
+function renderPage(){
+    for (var i = 0; i < localStorage.length; i++){
+        $("#"+localStorage.key(i)).children("textarea").val(localStorage.getItem(localStorage.key(i)));
+    }
+}
 $("button").on("click", function(event){
     var btn = event.target;
-    console.log(event.target);
-    //textField.siblings("textarea").text("hi");
-
+    var localRow = $(btn).closest(".row");
+    var text = localRow.children("textarea").val();
+    localStorage.setItem(localRow.attr("id"), text);
 });
+renderPage();
